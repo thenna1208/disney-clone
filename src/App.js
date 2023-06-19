7ab48282815acaec4e0d8b501f8eb69af975b15c
thenna1208 <thennavan.m@gmail.com>
2023-06-19 09:15:13 +0530

    deployed successfully

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './components/Login/Login';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Details';
 
 
 
@@ -16,6 +17,7 @@ function App() {
         
           <Route path='/' element={<Login />} ></Route>
           <Route path='/home' element = {<Home/>}></Route>
+          <Route path='/detail/:id' element = {<Detail/>}></Route>
           
         </Routes>
 
